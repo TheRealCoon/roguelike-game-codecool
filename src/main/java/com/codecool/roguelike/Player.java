0c4a5a8b2347d25bad3c557b6f6c1b590dca141b
adamfilep <adamfilep@gmail.com>
2022-10-14 17:46:54 +0200

    Add Player class

--- src/main/java/com/codecool/roguelike/Player.java
+++ src/main/java/com/codecool/roguelike/Player.java
@@ -6,4 +6,72 @@ package com.codecool.roguelike;
  */
 public class Player {
 
+    private String NAME;
+    private Race RACE;
+    private int health;
+    private int damage;
+    private int damageTaken;
+    private Coordinates coordinates;
+
+    public Player(String name, Coordinates coordinates) {
+        this.NAME = name;
+        RACE = Race.HUMAN;
+        health = 100;
+        damage = 10;
+        damageTaken = 10;
+        this.coordinates = coordinates;
+    }
+
+    public void moveUp() {
+        coordinates.setVerticalCoordinate(coordinates.getVerticalCoordinate() + 1);
+    }
+
+    public void moveDown() {
+    }
+
+    public void moveLeft() {
+    }
+
+    public void moveRight() {
+    }
+
+    public String getPlayerName() {
+        return NAME;
+    }
+
+    public Race getPlayerRace() {
+        return RACE;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getDamageTaken() {
+        return damageTaken;
+    }
+
+    public void setPlayerName(String name) {
+        this.NAME = name;
+    }
+
+    public void setPlayerRace(Race race) {
+        this.RACE = race;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setDamageTaken(int damageTaken) {
+        this.damageTaken = damageTaken;
+    }
 }
