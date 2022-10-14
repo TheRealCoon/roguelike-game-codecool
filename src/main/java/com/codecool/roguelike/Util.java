44f8442f202ee9f35aaaa2b73b767199d98418ae
adamfilep <adamfilep@gmail.com>
2022-10-14 17:50:23 +0200

    Add getInputString() to get Player name from user

--- src/main/java/com/codecool/roguelike/Util.java
+++ src/main/java/com/codecool/roguelike/Util.java
@@ -3,6 +3,7 @@ package com.codecool.roguelike;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Util {
@@ -20,5 +21,14 @@ public class Util {
 
     public static char getInputChar() throws IOException {
         return br.readLine().charAt(0);
+    }
+
+    public static String getInputString() throws IOException {
+        String userInput = "";
+        while (userInput.equals("")) {
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.nextLine();
+        }
+        return userInput;
     }
 }
