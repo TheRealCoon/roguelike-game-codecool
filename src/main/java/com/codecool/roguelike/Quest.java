86492cce1d0015e330f7fce82f56862b45dcd135
Norbert Gila <gila.nrbrt@gmail.com>
2022-10-17 20:02:25 +0200

    implemented basic Quest class

---
+++ src/main/java/com/codecool/roguelike/Quest.java
@@ -0,0 +1,57 @@
+package com.codecool.roguelike;
+
+public class Quest {// TODO a container to all killed enemies given to quest
+
+    private String name;
+    private String description;
+    private String objective;//TODO what type shall it be?
+    private Item reward;
+    private String progress;//TODO what type shall it be?
+
+    public Quest(String name, String description, String objective, Item reward) {
+        this.name = name;
+        this.description = description;
+        this.objective = objective;
+        this.reward = reward;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getObjective() {
+        return objective;
+    }
+
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
+
+    public Item getReward() {
+        return reward;
+    }
+
+    public void setReward(Item reward) {
+        this.reward = reward;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+}
