f6278df412ecd475301338514f71104abcf52418
Norbert Gila <gila.nrbrt@gmail.com>
2022-10-17 19:02:38 +0200

    implemented enum MobType

---
+++ src/main/java/com/codecool/roguelike/MobType.java
@@ -0,0 +1,17 @@
+package com.codecool.roguelike;
+
+public enum MobType {
+    ORC(1.2),
+    WOLF(1),
+    TROLL(1.5);
+
+    private final double statMultiplier;
+
+    MobType(double statMultiplier) {
+        this.statMultiplier = statMultiplier;
+    }
+
+    public double getStatMultiplier() {
+        return statMultiplier;
+    }
+}
