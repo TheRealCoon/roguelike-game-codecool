6cb9163cf7010579f8ec845f9062d30a9cbfbb97
Norbert Gila <gila.nrbrt@gmail.com>
2022-10-19 00:46:01 +0200

    implemented Interactable interface in Npc

--- src/main/java/com/codecool/roguelike/Npc.java
+++ src/main/java/com/codecool/roguelike/Npc.java
@@ -2,7 +2,7 @@ package com.codecool.roguelike;
 
 import java.util.List;
 
-public class Npc extends GameCharacter{
+public class Npc extends GameCharacter implements Interactable{
 
     private String message;
     private List<Quest> questList;
@@ -14,7 +14,17 @@ public class Npc extends GameCharacter{
     }
 
     private void fillQuestList(){
-        questList.add(new Quest("Kill them all!", "Hi fellow traveller! I have a quest for you! You shall kill all enemies in this room and I can give you a sword for it!", "Kill (2) enemies", new Item()));
+        questList.add(new Quest("Kill them all!",
+                "Hi fellow traveller! I have a quest for you! You shall kill all enemies in this room and I can give you THE KEY!",
+                "Kill (2) enemies",
+                new Item()));
+    }
+
+    public void interact(Player player){
+        Quest quest = questList.get(0);
+        quest.toString(); //TODO display to player
+        quest.setActive(true);
+
     }
 
 
