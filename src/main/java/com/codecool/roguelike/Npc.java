88f227948acaa25d9ff849238b3a20117de8b66b
Norbert Gila <gila.nrbrt@gmail.com>
2022-10-19 01:21:13 +0200

    added getActiveQuest() to Npc rename questList to quests

--- src/main/java/com/codecool/roguelike/Npc.java
+++ src/main/java/com/codecool/roguelike/Npc.java
@@ -2,10 +2,10 @@ package com.codecool.roguelike;
 
 import java.util.List;
 
-public class Npc extends GameCharacter implements Interactable{
+public class Npc extends GameCharacter implements Interactable {
 
     private String message;
-    private List<Quest> questList;
+    private List<Quest> quests;
 
     private static final char icon = 'N';
 
@@ -13,18 +13,26 @@ public class Npc extends GameCharacter implements Interactable{
         super(name, coordinates, icon);
     }
 
-    private void fillQuestList(){
-        questList.add(new Quest("Kill them all!",
+    private void fillQuestList() {
+        quests.add(new Quest("Kill them all!",
                 "Hi fellow traveller! I have a quest for you! You shall kill all enemies in this room and I can give you THE KEY!",
                 "Kill (2) enemies",
                 new Item()));
     }
 
-    public void interact(Player player){
-        Quest quest = questList.get(0);
+    public void interact(Player player) {
+        Quest quest = quests.get(0);
         quest.toString(); //TODO display to player
         quest.setActive(true);
+    }
 
+    public Quest getActiveQuest() {
+        for (Quest q : quests) {
+            if (q.isActive())
+                return q;
+        }
+
+        return null;
     }
 
 
